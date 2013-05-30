fdadd2301fc99d9af3a2cb7bd9fee8cee284a516
Jaime Febres <jaime.febres@gmail.com>
2013-05-29 20:04:31 -0500

    Checkpoint

--- src/main/scala/com/yougrade/QuizService.scala
+++ src/main/scala/com/yougrade/QuizService.scala
@@ -9,18 +9,31 @@ import DefaultJsonProtocol._
 
 trait QuizService extends HttpService {
   implicit val quizHeaderFormat = jsonFormat5(QuizHeader)
-  private val quizzesByLang = List(
-      QuizHeader(1,
+  implicit val quizAlternativeFormat = jsonFormat1(QuizAlternative)
+  implicit val quizQuestionFormat = jsonFormat3(QuizQuestion)  
+  implicit val quizFormat = jsonFormat2(Quiz)
+  
+  private val quizzes = List(
+      Quiz(QuizHeader(1,
           "Learning English with \"The Big Bang Theory\"",
           "Show your knowledge on the English language using videos from The Big Bang Theory",
           "http://img.youtube.com/vi/fRaUVp5DfRk/0.jpg",
-          "en")) 
+          "en"),
+          List(QuizQuestion(
+              "In this episode, Sheldon proposes to Raj a variation for the classic \"Jokempo\" game: instead of \"rock-paper-scissors\", the variation is \"rock-paper-scissors-lizard-spock\". In his explanation for the game rules, Sheldon points out that paper beats Spock. How can Spock be defeated by paper?",
+              "iapcKVn7DdY",
+              Nil))
+              )
+      )
+  
+  private val quizzesByLang = quizzes.map(_.header)
+  
   val quizServiceRoutes =    
     path("quizzes" / IntNumber) { id =>
         get {
           respondWithMediaType(`application/json`){
             complete {
-              quizzesByLang(0).toJson.prettyPrint
+              quizzes(0).toJson.prettyPrint
             }
           }
         }
@@ -33,7 +46,9 @@ trait QuizService extends HttpService {
           }
         }
       }
-    }
-  	
+    }  	
 }
 case class QuizHeader(id:Int,title:String,description:String,thumbnail:String,lang:String)
+case class QuizAlternative(text:String)
+case class QuizQuestion(description:String,video:String,alternatives:List[QuizAlternative])
+case class Quiz(header:QuizHeader,questions:List[QuizQuestion])
